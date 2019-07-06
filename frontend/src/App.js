ed019f4a46e6639131d6b82a678ecc16d5ea0480
Fabricio Loupias <fabricioloupias@Mac-de-CL.local>
2019-07-06 14:42:38 -0300

    mern-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+//Components
+import NavTop from './components/NavTop'
+
+function App() {
+  return (
+    <div className="App">
+      <NavTop />
+    </div>
+  );
+}
+
+export default App;
